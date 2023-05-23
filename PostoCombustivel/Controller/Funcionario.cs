1e9d315d8cf10009ba82409bd3d7a9f3c83d2005
Jussan Silva <jussan.silva@txtecnologia.com.br>
2023-05-23 18:47:25 -0300

    criando codificação da senha

--- PostoCombustivel/Controller/Funcionario.cs
+++ PostoCombustivel/Controller/Funcionario.cs
@@ -6,6 +6,8 @@
 */
 
 using Model;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Controller
 {
@@ -19,6 +21,7 @@ namespace Controller
             string email)
         {
             try{
+                CriptografaSenha(senha);
                 if (Model.Loja.BuscaLojaId(int.Parse(lojaId)) == null) throw new Exception("Erro ao cadastrar funcionário, loja não encontrada");
                 Model.Funcionario funcionario = new Model.Funcionario(
                     nome,
@@ -43,6 +46,7 @@ namespace Controller
         {
             try
             {
+                CriptografaSenha(senha);
                 if (Model.Funcionario.BuscaFuncionarioPorId(int.Parse(funcionarioId)) == null) throw new Exception("Erro ao alterar funcionário, funcionário não encontrado");
                 if (Model.Loja.BuscaLojaId(int.Parse(lojaId)) == null) throw new Exception("Erro ao alterar funcionário, loja não encontrada");
                 return Model.Funcionario.UpdateFuncionario(
@@ -86,6 +90,27 @@ namespace Controller
                 Model.Funcionario.DeleteFuncionario(int.Parse(funcionarioId));
             } catch (Exception) {
                 throw new Exception("Erro ao excluir funcionário");
+            }
+        }
+
+        private static string CriptografaSenha(string senha)
+        {
+            try
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(senha);
+                    byte[] hash = sha256.ComputeHash(bytes);
+
+                    StringBuilder builder = new StringBuilder();
+                    for (int i = 0; i < hash.Length; i++)
+                    {
+                        builder.Append(hash[i].ToString("X3"));
+                    }
+                    return builder.ToString();
+                }
+            } catch (Exception) {
+                throw new Exception("Erro ao criptografar senha");
             }
         }
     }
